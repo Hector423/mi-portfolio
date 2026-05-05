41014aa5fec909917e9727e423342fb2db2b6987
Hector <hectorvallveg@gmail.com>
2026-05-05 17:51:21 +0200

    cambios en writeups

---
+++ docusaurus.config.js
@@ -0,0 +1,106 @@
+// @ts-check
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Héctor | Mi portfolio',
+  tagline: 'Writeups de Ciberseguridad',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://Hector423.github.io',
+  // La barra final es fundamental para GitHub Pages
+  baseUrl: '/mi-portfolio/',
+
+  organizationName: 'Hector423', 
+  projectName: 'mi-portfolio', 
+
+  // Cambiamos a 'warn' para que no aborte el despliegue si hay algún link roto
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          editUrl: 'https://github.com/Hector423/mi-portfolio/tree/main/',
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Héctor Portfolio',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Writeups',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/Hector423/mi-portfolio',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'DockerLabs',
+            items: [
+              {
+                label: 'Máquinas Muy Fáciles',
+                // Ajustado a tu ruta real según la imagen
+                to: '/docs/DockerLabs/Máquinas muy faciles/trust', 
+              },
+              {
+                label: 'Máquinas Fáciles',
+                to: '/docs/DockerLabs/Máquinas fáciles/Mirame',
+              },
+            ],
+          },
+          {
+            title: 'Contacto',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Hector423',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Héctor. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
